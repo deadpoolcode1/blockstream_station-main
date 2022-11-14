cee99ada5fc8ef5af1aa123a7e22066947841c59
egolan770 <egolan@blockstream.com>
2022-11-14 13:22:55 +0200

    14/11/2022 : fixed few bugs in examples code

--- examples/zeromq_server/server.cpp
+++ examples/zeromq_server/server.cpp
@@ -14,6 +14,7 @@ int main () {
     zmq::socket_t socket (context, ZMQ_REP);
     //socket.bind ("tcp://*:5555");
     socket.bind ("ipc:///tmp/test");
+    std::cout << "Server started "  << std::endl;
     char buffer[256];
     while (true) {
         zmq::message_t request;
@@ -21,16 +22,18 @@ int main () {
         //  Wait for next request from client
         zmq_recv(socket, buffer, sizeof(buffer), 0);
         std::string temp(buffer);
-        std::cout << "Server Receive " + temp << std::endl;
-
         //  Do some 'work'
-        sleep (1);
+        std::cout << "Server Receive " + temp << std::endl;
+        //sleep (1);
 
         //  Send reply back to client
-        zmq::message_t reply (5);
-        std::cout << "Sending Hello" << std::endl;
-        memcpy ((void *) reply.data (), "World", 5);
-        socket.send (reply);
+        //zmq::message_t reply (5);
+	    char reply[] = "World";
+        std::string temp2(reply);
+        std::cout << "Sending reply: " + temp2 << std::endl;
+        //memcpy ((void *) reply.data (), "World", 5);
+        //socket.send (reply);
+	    zmq_send(socket, strdup(reply), strlen(reply), 0);
     }
     return 0;
 }
