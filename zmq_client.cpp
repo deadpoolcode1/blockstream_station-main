7924def9419a21bcb8478e7af2b2ecd7aeaa91d5
Ilan Ganor <ilan@kamacode.com>
2023-07-30 13:50:10 +0300

    moved to support tcp messages to support external communication

--- zmq_client.cpp
+++ zmq_client.cpp
@@ -1,31 +1,75 @@
-#include <zmq.hpp>
-#include <string>
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
-    if(argc < 2) {
-        std::cout << "Please provide a message to send as the first argument." << std::endl;
-        return -1;
+void handleClient(int clientSocket) {
+    char buffer[1024];
+    int bytesRead;
+
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[bytesRead] = '\0';
+        std::cout << "Received message: " << buffer << std::endl;
+
+        // Send ACK back to the client
+        const char* ackMessage = "ACK";
+        send(clientSocket, ackMessage, strlen(ackMessage), 0);
     }
 
-    std::string messageToSend(argv[1]);
+    close(clientSocket);
+}
 
-    zmq::context_t context(1);
-    zmq::socket_t socket(context, ZMQ_REQ);
+int main() {
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t clientAddrSize = sizeof(clientAddr);
 
-    std::cout << "Connecting to server..." << std::endl;
-    socket.connect("ipc:///tmp/test");
+    // Create socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Error creating socket" << std::endl;
+        return 1;
+    }
 
-    zmq::message_t request(messageToSend.size());
-    memcpy(request.data(), messageToSend.data(), messageToSend.size());
-    std::cout << "Sending message " << messageToSend << "..." << std::endl;
-    socket.send(request, zmq::send_flags::none);
+    // Bind socket to port 5555
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(5555);
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error binding to port 5555" << std::endl;
+        close(serverSocket);
+        return 1;
+    }
 
-    zmq::message_t reply;
-    socket.recv(reply, zmq::recv_flags::none);
+    // Start listening for incoming connections
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Error listening for connections" << std::endl;
+        close(serverSocket);
+        return 1;
+    }
 
-    std::string received_message(static_cast<char*>(reply.data()), reply.size());
-    std::cout << "Received message: " << received_message << std::endl;
+    std::cout << "Listening on TCP port 5555..." << std::endl;
 
+    // Accept incoming connections and handle them using threads
+    std::vector<std::thread> threads;
+    while (true) {
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        if (clientSocket == -1) {
+            std::cerr << "Error accepting connection" << std::endl;
+            continue;
+        }
+
+        threads.emplace_back(handleClient, clientSocket);
+    }
+
+    // Join all threads (this part will never be reached in this simple example)
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    close(serverSocket);
     return 0;
 }
